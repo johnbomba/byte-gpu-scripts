335934b9fc9e7d40f2c987b2a2ee4ccb5c1f178b
johnbomba <bomba.john@gmail.com>
2019-12-03 13:16:01 -0500

    added run this first and update rsync script

--- rsync-ssh.sh
+++ rsync-ssh.sh
@@ -4,6 +4,7 @@ read -p "Enter username: " USERNAME
 
 sudo rsync --archive --chown=$USERNAME:$USERNAME ssh-tunnel.sh /home/$USERNAME
 
-echo "USERNAME:$USERNAME" >> connection-notes.txt
-
 sudo rsync --archive --chown=$USERNAME:$USERNAME connection-notes.txt /home/$USERNAME
+
+sudo sed -i "s/<username>/$USERNAME/" /home/$USERNAME/connection-notes.txt
+
